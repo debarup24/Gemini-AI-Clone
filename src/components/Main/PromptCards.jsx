d0a41f4a72de34ada8697600ee0be9a26729c606
Debarup <87480898+debarup24@users.noreply.github.com>
2024-12-28 22:20:35 +0530

    Gemini clone

--- src/components/Main/PromptCards.jsx
+++ src/components/Main/PromptCards.jsx
@@ -6,7 +6,7 @@ const PromptCards = () => {
     <div className="flex flex-row w-[900px] m-auto gap-4 p-3">
       <div className="h-52 p-4  relative cursor-pointer bg-gray-200 hover:bg-slate-300 rounded-xl shadow-md shadow-cyan-300">
         <p className="text-gray-600 font-semibold mt-1">
-          Suggest beautiful places to visit in New Delhi
+          Suggest beautiful places to visit in Jaipur, Rajasthan, India
         </p>
         <img
           className="w-9 p-1 absolute bg-slate-50 rounded-full bottom-2 right-2"
