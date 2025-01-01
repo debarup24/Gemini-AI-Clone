9f31a3dd20c30871d54bfd0c9558e231d3ab3c94
Debarup <87480898+debarup24@users.noreply.github.com>
2025-01-01 12:51:16 +0530

    gemini clone

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,4 +1,4 @@
-
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY ;
 
 import {
     GoogleGenerativeAI,
@@ -7,7 +7,7 @@ import {
   } from "@google/generative-ai" ;
 
   
-  const apiKey = import.meta.env.VITE_GEMINI_API_KEY ;
+  
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
